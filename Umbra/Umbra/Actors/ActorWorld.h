e5107dedaef6504f0f34b98cdfcc7bd5067a9bbf
Jack Bramley <s242106@students.aie.edu.au>
2025-06-19 14:30:43 +1000

    Completed Actor Classes - Actor - ActorTransform - ActorWorld - Level

--- Umbra/Umbra/Actors/ActorWorld.h
+++ Umbra/Umbra/Actors/ActorWorld.h
@@ -1,5 +1,32 @@
 #pragma once
+
+#include <functional>
+#include <vector>
+
+using std::function;
+using std::pair;
+using std::vector;
+
+class Actor;
+
 class ActorWorld
 {
+	friend class Level;
+
+public:
+	void Spawn(Actor* actor);
+	void Destroy(Actor* actor);
+
+private:
+	vector<Actor*> m_actors;
+	vector<pair<function<void(Actor*)>, Actor*>> m_listChanges;
+
+private:
+	ActorWorld();
+	~ActorWorld();
+
+private:
+	void Tick(float dt);
+	void Render() const;
 };
 
